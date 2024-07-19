ff4f1d014d554c4861a0a792b24bba05a7bcb364
Arun Vinod K <arunvinod9497@gmail.com>
2024-07-19 23:59:35 +0530

    Learned title in advance for Metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,12 @@
-export default function Blog(){
-    return <h1>Blog Page</h1>
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+//   title: "Blog", works with template title
+title:{
+    absolute:"Blog"
+}
+};
+
+export default function Blog() {
+  return <h1>Blog Page</h1>;
 }
