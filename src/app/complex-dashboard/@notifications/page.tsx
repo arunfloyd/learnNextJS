dd2d898b79b700517a55633c536d45e026330501
Arun Vinod K <arunvinod9497@gmail.com>
2024-07-22 15:53:38 +0530

    If there is unmatched route it will move to 404 page so to handle we can create a default file to manage it

--- src/app/complex-dashboard/@notifications/page.tsx
+++ src/app/complex-dashboard/@notifications/page.tsx
@@ -1,5 +1,10 @@
 import Card from "@/components/card";
-
+import Link from "next/link";
 export default function Notifications() {
-  return <Card>Notifications</Card>;
+  return (
+    <Card>
+      <div>Notifications</div>
+      <Link href="/complex-dashboard/archived">Archived</Link>
+    </Card>
+  );
 }
---
+++ src/app/complex-dashboard/default.tsx
@@ -0,0 +1,3 @@
+export default function DefaultComplexDashBoardPage() {
+  return <h1>Complex Dashboard</h1>;
+}
