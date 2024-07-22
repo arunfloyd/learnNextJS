dff54754477289786205588bbf46e707c5832f1d
Arun Vinod K <arunvinod9497@gmail.com>
2024-07-22 13:11:04 +0530

    Handled unexpected error and shows the error message to the user

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,20 +1,20 @@
 import { Metadata } from "next";
 import Link from "next/link";
 
-export const generrateMetadata = async ({
-  params,
-}: {
-  params: { productId: string };
-}): Promise<Metadata> => {
-  const title = await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(`iPhone ${params.productId}`);
-    }, 100);
-  });
-  return {
-    title: `Product ${title}`,
-  };
-};
+// export const generrateMetadata = async ({
+//   params,
+// }: {
+//   params: { productId: string };
+// }): Promise<Metadata> => {
+//   const title = await new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(`iPhone ${params.productId}`);
+//     }, 100);
+//   });
+//   return {
+//     title: `Product ${title}`,
+//   };
+// };
 
 export default function ProductDetails({
   params,
