16381f8ff7c49284f364a560c5d8c11203fc5a58
Arun Vinod K <arunvinod9497@gmail.com>
2024-07-23 12:34:16 +0530

    So in this video we have learned to make update on the data using PATCH request

---
+++ src/app/comments/[id]/route.ts
@@ -0,0 +1,24 @@
+import { comments } from "../data";
+export async function GET(
+  _request: Request,
+  { params }: { params: { id: string } }
+) {
+  const comment = comments.find(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  return Response.json(comment);
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const { text } = body;
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  comments[index].text = text;
+
+  return Response.json(comments[index]);
+}
