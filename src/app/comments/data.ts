dbb6672483ba2cbf108432588e58796c5b4f1136
Arun Vinod K <arunvinod9497@gmail.com>
2024-07-23 11:57:48 +0530

    Handling GET Request and fetched the data using the thunder API instead of UI client call

---
+++ src/app/comments/data.ts
@@ -0,0 +1,13 @@
+export const comments = [
+  {
+    id: 1,
+    text: "This is the first comment",
+  },
+  {
+    id: 2,
+    text: "This is the second comment",
+  },
+  {
+    text: "This is the Third comment",
+  },
+];
